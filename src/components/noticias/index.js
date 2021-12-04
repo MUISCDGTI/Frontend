53bcaf6915a0343fdb60e5b80f49194d61c3d52b
José Manuel Muñiz Peña <josemamup@gmail.com>
2021-12-04 16:27:23 +0100

    Lista estática de noticias

--- src/components/noticias/index.js
+++ src/components/noticias/index.js
@@ -1,16 +1,76 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import './index.css';
-import { Card } from 'antd';
+import { Card, List, Avatar, Space } from 'antd';
+import { MessageOutlined, LikeOutlined, StarOutlined } from '@ant-design/icons';
 
 const Noticias = () => {
+    const newsList = [];
+
+    newsList.push({
+        href: 'https://imagenes.elpais.com/resizer/GLfCZ8oOlfil2w-XpWWl99j8sT8=/1960x0/cloudfront-eu-central-1.images.arcpublishing.com/prisa/W5O4QHHCMBHQ5DHXSDSIPAVNFY.jpg',
+        title: `Luces y sombras del segundo final de La casa de papel`,
+        avatar: 'https://joeschmoe.io/api/v1/random',
+        description:
+            'La serie se despide convertida en el mayor éxito global de la ficción televisiva española',
+        content:
+            '',
+    })
+
+    for (let i = 0; i < 23; i++) {
+        newsList.push({
+            href: 'https://ant.design',
+            title: `Luces y sombras del segundo final de La casa de papel`,
+            avatar: 'https://joeschmoe.io/api/v1/random',
+            description:
+                'La serie se despide convertida en el mayor éxito global de la ficción televisiva española',
+            content:
+                '',
+        });
+    }
+
+    const IconText = ({ icon, text }) => (
+        <Space>
+            {React.createElement(icon)}
+            {text}
+        </Space>
+    );
+
+
     return (
         <div>
-            <Card title="Default size card" style={{ width: 300 }}>
-                <p>Card content</p>
-                <p>Card content</p>
-                <p>Card content</p>
-            </Card>
+            <List
+                itemLayout="vertical"
+                size="large"
+                pagination={{
+                    onChange: page => {
+                        console.log(page);
+                    },
+                    pageSize: 3,
+                }}
+                dataSource={newsList}
+
+                renderItem={item => (
+                    <List.Item
+                        key={item.title}
+                        extra={
+                            <img
+                                width={272}
+                                alt="logo"
+                                src="https://imagenes.elpais.com/resizer/GLfCZ8oOlfil2w-XpWWl99j8sT8=/1960x0/cloudfront-eu-central-1.images.arcpublishing.com/prisa/W5O4QHHCMBHQ5DHXSDSIPAVNFY.jpg"
+                            />
+                        }
+                    >
+                        <List.Item.Meta
+                            avatar={<Avatar src={item.avatar} />}
+                            title={<a href={item.href}>{item.title}</a>}
+                            description={item.description}
+                        />
+                        {item.content}
+                    </List.Item>
+                )}
+            />
+
         </div>
     );
 }
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Noticias from '../components/noticias';
 // require normalized overrides
 import MainPage from '../pages/index';
 
@@ -9,6 +10,8 @@ function Router() {
             <div>
                 <Routes>
                     <Route path="/" element={<MainPage/>}>
+                    </Route>
+                    <Route path="/newsList" element={<Noticias/>}>
                     </Route>
                 </Routes>
             </div>
