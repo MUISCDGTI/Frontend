daff019da080d30ff9444c367f8c3d806e98cb17
GermanMT <germanoctako@gmail.com>
2021-12-16 17:18:57 +0100

    Added ratings button and fixed menu

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import './index.css';
+import { Link } from 'react-router-dom';
 import { Menu } from 'antd';
 
 const CustomHeader = () => {
     return (
         <div>
             <div className="logo" >
-                Rotten Potatoes
+                <Link to="/" >
+                    Rotten Potatoes
+                </Link>
             </div>
             <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
                 <Menu.Item key='films'>Películas</Menu.Item>
                 <Menu.Item key='news'>Noticias</Menu.Item>
                 <Menu.Item key='user'>Usuario</Menu.Item>
+                <Link to="/ratings" >
+                <Menu.Item key='ratings'>Valoraciones</Menu.Item>
+                </Link>
             </Menu>
         </div>
     );
