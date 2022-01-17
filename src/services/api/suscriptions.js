23abc5ddbf8f91addbdfb81fcb4c33a4486c6cf9
Alvaro <48862983+AlvaroNavarroMora@users.noreply.github.com>
2022-01-17 18:30:59 +0100

    update apikey

--- src/services/api/suscriptions.js
+++ src/services/api/suscriptions.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const endPoint = 'https://suscripciones-amaliof96.cloud.okteto.net/api/v1/suscripciones';
-const apikey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InJvdHRlbnBvdGF0b2VzIiwiaWF0IjoxNjQyNDE5Mzk3fQ.TrSSr2aRHzQyQTsVg3DVwM0NvvzixVcJw_MwyVcwy8s";
+const apikey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InJvdHRlbnBvdGF0b2VzIiwiaWF0IjoxNjQyNDQwNTY5fQ.CjvfpTYgblkGEEwluHaVTieOjGL3fh_CmWy2zHh0mGk";
 export const getSuscriptions = (email, categoria) => axios.get(
     endPoint, { params:  {email: email, category:categoria, apikey: apikey} },
 ).then((res) => res).catch((error) => error.response);
